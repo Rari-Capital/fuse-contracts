c92c5ae6a7332f1663d6582bb68e982d55787aa0
davidlucid <davidlucidsl@gmail.com>
2021-02-28 21:46:22 -0600

    Development migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -20,7 +20,7 @@ module.exports = async function(deployer, network, accounts) {
   }
   
   // Deploy FusePoolDirectory
-  await deployProxy(FusePoolDirectory, [true, [["live", "live-fork"].indexOf(network) >= 0 ? process.env.LIVE_OWNER : process.env.DEVELOPMENT_ADDRESS]], { deployer, unsafeAllowCustomTypes: true });
+  await deployProxy(FusePoolDirectory, [["live", "live-fork"].indexOf(network) >= 0, ["live", "live-fork"].indexOf(network) >= 0 ? [process.env.LIVE_OWNER] : []], { deployer, unsafeAllowCustomTypes: true });
   
   // Deploy FuseSafeLiquidator
   await deployer.deploy(FuseSafeLiquidator);
