6f546e78edde8c9aa6a3fb7c6aed9d62cbe85f2f
davidlucid <davidlucidsl@gmail.com>
2021-03-13 17:47:39 -0700

    Better Keep3rPriceOracle (KPO-02M)

--- contracts/external/keep3r/Keep3rV1Oracle.sol
+++ contracts/external/keep3r/Keep3rV1Oracle.sol
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.6.12;
 
-interface Keep3rV1Oracle {
-    function current(address tokenIn, uint amountIn, address tokenOut) external view returns (uint amountOut);
+abstract contract Keep3rV1Oracle {
+    struct Observation {
+        uint timestamp;
+        uint price0Cumulative;
+        uint price1Cumulative;
+    }
+
+    function factory() external pure virtual returns (address);
+    mapping(address => Observation[]) public observations;
+    function observationLength(address pair) external view virtual returns (uint);
 }
