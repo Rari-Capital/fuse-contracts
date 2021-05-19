7666b3fe4409e846469b6e3f9e264cfaf5be3bfd
davidlucid <davidlucidsl@gmail.com>
2021-05-18 20:47:43 -0500

    Create FixedEthPriceOracle.sol

---
+++ contracts/oracles/FixedEthPriceOracle.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.6.12;
+
+import "@openzeppelin/contracts-upgradeable/math/SafeMathUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+
+import "../external/compound/PriceOracle.sol";
+import "../external/compound/CToken.sol";
+import "../external/compound/CErc20.sol";
+
+import "./BasePriceOracle.sol";
+
+/**
+ * @title FixedEthPriceOracle
+ * @notice Returns fixed ETH prices.
+ * @dev Implements `PriceOracle` and `BasePriceOracle`.
+ * @author David Lucid <david@rari.capital> (https://github.com/davidlucid)
+ */
+contract FixedEthPriceOracle is PriceOracle, BasePriceOracle {
+    using SafeMathUpgradeable for uint256;
+
+    /**
+     * @dev Returns the price in ETH of `underlying` (implements `BasePriceOracle`).
+     */
+    function price(address underlying) external override view returns (uint) {
+        return 1e18;
+    }
+
+    /**
+     * @notice Returns the price in ETH of the token underlying `cToken`.
+     * @dev Implements the `PriceOracle` interface for Fuse pools (and Compound v2).
+     * @return Price in ETH of the token underlying `cToken`, scaled by `10 ** (36 - underlyingDecimals)`.
+     */
+    function getUnderlyingPrice(CToken cToken) external override view returns (uint) {
+        // Get underlying token address
+        address underlying = CErc20(address(cToken)).underlying();
+
+        // Format and return price
+        return uint256(1e36).div(10 ** uint256(ERC20Upgradeable(underlying).decimals()));
+    }
+}
