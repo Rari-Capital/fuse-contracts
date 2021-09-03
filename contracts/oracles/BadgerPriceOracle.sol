3e5111de705843a06603982f76b5dcc27061e460
davidlucid <davidlucidsl@gmail.com>
2021-09-02 20:06:30 -0500

    Fix QSP-3 Oracle prices could be stale

--- contracts/oracles/BadgerPriceOracle.sol
+++ contracts/oracles/BadgerPriceOracle.sol
@@ -80,16 +80,23 @@ contract BadgerPriceOracle is PriceOracle, BasePriceOracle {
      */
     function _price(address token) internal view returns (uint) {
         if (token == address(BBADGER)) {
-            (, int256 badgerEthPrice, , , ) = BADGER_ETH_FEED.latestRoundData();
-            return badgerEthPrice >= 0 ? uint256(badgerEthPrice).mul(BBADGER.getPricePerFullShare()).div(1e18) : 0;
+            (uint80 roundId, int256 badgerEthPrice, , , uint80 answeredInRound) = BADGER_ETH_FEED.latestRoundData();
+            require(answeredInRound == roundId, "Chainlink round timed out.");
+            return badgerEthPrice > 0 ? uint256(badgerEthPrice).mul(BBADGER.getPricePerFullShare()).div(1e18) : 0;
         } else if (token == address(BDIGG)) {
-            (, int256 diggBtcPrice, , , ) = DIGG_BTC_FEED.latestRoundData();
-            (, int256 btcEthPrice, , , ) = BTC_ETH_FEED.latestRoundData();
+            (uint80 roundId, int256 diggBtcPrice, , , uint80 answeredInRound) = DIGG_BTC_FEED.latestRoundData();
+            require(answeredInRound == roundId, "Chainlink round timed out.");
+            if (diggBtcPrice < 0) return 0;
+            int256 btcEthPrice;
+            (roundId, btcEthPrice, , , answeredInRound) = BTC_ETH_FEED.latestRoundData();
+            require(answeredInRound == roundId, "Chainlink round timed out.");
+            if (btcEthPrice < 0) return 0;
             uint256 bDiggDiggPrice = IDigg(BDIGG.token()).sharesToFragments(BDIGG.shares().div(BDIGG.totalSupply()).mul(1e18));
-            return diggBtcPrice >= 0 && btcEthPrice >= 0 ? uint256(diggBtcPrice).mul(uint256(btcEthPrice)).div(1e8).mul(bDiggDiggPrice).div(1e9) : 0;
+            return bDiggDiggPrice > 0 ? uint256(diggBtcPrice).mul(uint256(btcEthPrice)).div(1e8).mul(bDiggDiggPrice).div(1e9) : 0;
         } else if (token == address(IBBTC)) {
-            (, int256 btcEthPrice, , , ) = BTC_ETH_FEED.latestRoundData();
-            return btcEthPrice >= 0 ? uint256(btcEthPrice).mul(IBBTC.pricePerShare()).div(1e18) : 0;
+            (uint80 roundId, int256 btcEthPrice, , , uint80 answeredInRound) = BTC_ETH_FEED.latestRoundData();
+            require(answeredInRound == roundId, "Chainlink round timed out.");
+            return btcEthPrice > 0 ? uint256(btcEthPrice).mul(IBBTC.pricePerShare()).div(1e18) : 0;
         } else revert("Invalid token address passed to BadgerPriceOracle.");
     }
 }
