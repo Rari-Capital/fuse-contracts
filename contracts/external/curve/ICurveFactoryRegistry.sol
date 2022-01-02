5b0804c0075981de35357a75478f1f2f6a55982b
Joey Santoro <joseph.santoro@duke.edu>
2022-01-01 17:08:29 -0800

    factory

--- contracts/external/curve/ICurveFactoryRegistry.sol
+++ contracts/external/curve/ICurveFactoryRegistry.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.6.12;
 
-interface ICurveRegistry {
+interface ICurveFactoryRegistry {
     function get_n_coins(address lp) external view returns (uint);
     function get_coins(address pool) external view returns (address[4] memory);
 }
